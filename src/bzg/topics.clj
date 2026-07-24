0db7f78dd02a8d72bf5e6b534e6bd3d91c98ef8f
Bastien Guerry <bzg@gnu.org>
2026-07-24 09:56:01 +0200

    fix: Escape double quotes in escapeHtml

--- src/bzg/topics.clj
+++ src/bzg/topics.clj
@@ -449,7 +449,7 @@ table { margin-bottom: 2rem; }")
   function escapeHtml(text) {
     const div = document.createElement('div');
     div.textContent = text;
-    return div.innerHTML;
+    return div.innerHTML.replace(/\"/g, '&quot;');
   }
 
   function normalizeText(text) {
